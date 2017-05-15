d0c8cd3d7e4343707924b212250711c527270c92
andry81 <andry@inbox.ru>
2017-05-15 00:01:47 +0000

    * svn tests preparation
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/scripts/trunk@13 c83cf141-d9d7-4975-98f5-23f3ee83e49f

--- TestsExternal/batscript/__init__.bat
+++ TestsExternal/batscript/__init__.bat
@@ -1,12 +1,15 @@
 @echo off
 
-if "%__NEST_LVL%" == "" set __NEST_LVL=0
+rem exit if already initialized
+if 0%__INIT__% NEQ 0 exit /b 0
 
-if %__NEST_LVL% GTR 0 exit /b 0
-
-if "%CONTOOLS_ROOT%" == "" set "CONTOOLS_ROOT=%~dp0..\..\tools"
+if "%CONTOOLS_ROOT%" == "" set "CONTOOLS_ROOT=%~dp0..\..\Tools"
 set "CONTOOLS_ROOT=%CONTOOLS_ROOT:\=/%"
 if "%CONTOOLS_ROOT:~-1%" == "/" set "CONTOOLS_ROOT=%CONTOOLS_ROOT:~0,-1%"
+
+if "%CONFIG_ROOT%" == "" set "CONFIG_ROOT=%~dp0..\..\Config"
+set "CONFIG_ROOT=%CONFIG_ROOT:\=/%"
+if "%CONFIG_ROOT:~-1%" == "/" set "CONFIG_ROOT=%CONFIG_ROOT:~0,-1%"
 
 rem To avoid interference with already installed versions of tools we have to reset
 rem entire environment into initial state to just after the Windows install.
@@ -63,33 +66,43 @@ set "CONTOOLS_ROOT=%CONTOOLS_ROOT%"
 set "WINVER_VALUE="
 set "OSNAME="
 echo %~nx0: Resetting environment to defaults...
-call "%%CONTOOLS_ROOT%%/Tools/resetenv.bat" -p -e "%%~dp0..\..\Config\env\%%~1" >nul
+call "%%CONTOOLS_ROOT%%/resetenv.bat" -p -e "%%CONFIG_ROOT%%/env/%%~1" >nul
 echo.
 
-rem builtin variables
-set __NEST_LVL=0
-
-set __PASSED_TESTS=0
-set __OVERALL_TESTS=0
-
+rem basic variables
 set "TESTS_ROOT=%~dp0"
 set "TESTS_ROOT=%TESTS_ROOT:\=/%"
 if "%TESTS_ROOT:~-1%" == "/" set "TESTS_ROOT=%TESTS_ROOT:~0,-1%"
 
-if "%CONTOOLS_ROOT%" == "" set "CONTOOLS_ROOT=%~dp0..\..\tools"
-set "CONTOOLS_ROOT=%CONTOOLS_ROOT:\=/%"
-if "%CONTOOLS_ROOT:~-1%" == "/" set "CONTOOLS_ROOT=%CONTOOLS_ROOT:~0,-1%"
+if "%TEST_SCRIPTS_ROOT%" == "" set "TEST_SCRIPTS_ROOT=%TESTS_ROOT%/../../Scripts"
+set "TEST_SCRIPTS_ROOT=%TEST_SCRIPTS_ROOT:\=/%"
+if "%TEST_SCRIPTS_ROOT:~-1%" == "/" set "TEST_SCRIPTS_ROOT=%TEST_SCRIPTS_ROOT:~0,-1%"
 
-if "%GNUWIN32_ROOT%" == "" set "GNUWIN32_ROOT=%CONTOOLS_ROOT%/gnuwin32"
-set "GNUWIN32_ROOT=%GNUWIN32_ROOT:\=/%"
-if "%GNUWIN32_ROOT:~-1%" == "/" set "GNUWIN32_ROOT=%GNUWIN32_ROOT:~0,-1%"
+rem external tools root directory
+call :ABSPATH EXTERNAL_TOOLS_ROOT "%%TESTS_ROOT%%/../../ToolsExternal"
 
-if "%SVNCMD_TOOLS_ROOT%" == "" set "SVNCMD_TOOLS_ROOT=%CONTOOLS_ROOT%/scm/svn"
-set "SVNCMD_TOOLS_ROOT=%SVNCMD_TOOLS_ROOT:\=/%"
-if "%SVNCMD_TOOLS_ROOT:~-1%" == "/" set "SVNCMD_TOOLS_ROOT=%SVNCMD_TOOLS_ROOT:~0,-1%"
+rem python bin root
+call :ABSPATH TEST_PYTHON_BIN_ROOT "%%EXTERNAL_TOOLS_ROOT%%/python/python-3.x-win32"
 
-rem load rest variables
-call "%%CONTOOLS_ROOT%%/setvarsfromfile.bat" "%%~dp0init.vars"
+rem python executable path
+call :ABSPATH TEST_PYTHON_EXE "%%TEST_PYTHON_BIN_ROOT%%/python.exe"
+
+rem python pytest module command line prefix
+call :ABSPATH TEST_PYTHON_PYTEST_CMD_LINE_PREFIX "%%TEST_PYTHON_EXE%%" -B -m pytest
+
+rem python search paths
+set "PYTHONPATH=%%TESTS_ROOT:\=/%%/pyscripts;%%TESTS_ROOT:\=/%%/pytests"
+
+rem nest level to call tests end logic in case of exit from a batch file with 0 nest level
+if "%__NEST_LVL%" == "" set __NEST_LVL=0
 
 rem declare initialized
 set __INIT__=1
+
+exit /b 0
+
+:ABSPATH
+if "%~3" == "" (
+  set "%~1=%~dpf2"
+) else set %~1="%~dpf2" %3 %4 %5 %6 %7 %8 %9
+exit /b 0
--- TestsExternal/batscript/test_all.bat
+++ TestsExternal/batscript/test_all.bat
@@ -1,18 +1,19 @@
 @echo off
 
-rem Create local variable's stack
 setlocal
 
-call "%%~dp0__init__.bat"
+call "%%~dp0__init__.bat" || goto :EOF
 
 set /A __NEST_LVL+=1
 
-call "%%TESTS_ROOT%%/test_scm_svn__svn_changeset.bat"
+call "%%TESTS_ROOT%%/test_svn_changeset.bat"
+set LASTERROR=%ERRORLEVEL%
 
 set /A __NEST_LVL-=1
 
-if %__NEST_LVL%0 EQU 0 (
-  echo    %__PASSED_TESTS% of %__OVERALL_TESTS% tests is passed.
+if %__NEST_LVL% LEQ 0 (
   echo.^
   pause
 )
+
+exit /b %LASTERROR%
