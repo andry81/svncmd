e452411ccac4e6fff7cd08a1d8bdcdbdaf592d81
andry81 <andry@inbox.ru>
2017-06-10 17:04:00 +0000

    * fixed: control external records separately to versioned/unversioned items
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/scripts/trunk@39 c83cf141-d9d7-4975-98f5-23f3ee83e49f

--- Scripts/svn_has_changes.bat
+++ Scripts/svn_has_changes.bat
@@ -7,9 +7,15 @@ rem   Script do filter SVN status for changed versioned items or unversioned
 rem   items.
 
 rem Flags:
-rem  -stat-exclude-? - exclude status lines for unversionned files (? prefixed) from "svn status" output.
-rem  -stat-exclude-versioned - exclude status lines for versionned files (non ? prefixed) from "svn status" output.
-rem     Versioned items has inclusion priority over unversioned items.
+rem  -r - process externals recursively.
+rem  -stat-exclude-? - exclude status lines for items not under version
+rem     control (? prefixed) from "svn status" output.
+rem  -stat-include-X - include status lines for unversioned directories created
+rem     by an externals definition (X prefixed) from "svn status" output.
+rem  -stat-exclude-versioned - exclude status lines for versioned files
+rem     (not ? or X prefixed) from "svn status" output.
+rem  By default, script does not print externals definition status lines
+rem  (X prefixed) and does not use recursion on them.
 
 rem Examples:
 rem 1. call svn_has_changes.bat branch/current subdir/project1
@@ -28,7 +34,13 @@ set "?~nx0=%~nx0"
 
 rem exclude unversioned items
 set FLAG_SVN_STATUS_EXCLUDE_?=0
-rem ignore externals
+set "FLAG_TEXT_SVN_STATUS_INCLUDE_?=?"
+set "FLAG_TEXT_SVN_STATUS_EXCLUDE_?="
+rem show externals status (status with the X character)
+set FLAG_SVN_STATUS_INCLUDE_X=0
+set "FLAG_TEXT_SVN_STATUS_INCLUDE_X="
+set "FLAG_TEXT_SVN_STATUS_EXCLUDE_X=X"
+rem read status inexternals recursively (by default, ignore externals status)
 set FLAG_SVN_EXTERNALS_RECURSIVE=0
 set "FLAG_TEXT_SVN_IGNORE_EXTERNALS=--ignore-externals"
 rem exclude versioned changes
@@ -45,6 +57,12 @@ if not "%FLAG:~0,1%" == "-" set "FLAG="
 if not "%FLAG%" == "" (
   if "%FLAG%" == "-stat-exclude-?" (
     set FLAG_SVN_STATUS_EXCLUDE_?=1
+    set "FLAG_TEXT_SVN_STATUS_INCLUDE_?="
+    set "FLAG_TEXT_SVN_STATUS_EXCLUDE_?=?"
+  ) else if "%FLAG%" == "-stat-include-X" (
+    set FLAG_SVN_STATUS_INCLUDE_X=1
+    set "FLAG_TEXT_SVN_STATUS_INCLUDE_X=X"
+    set "FLAG_TEXT_SVN_STATUS_EXCLUDE_X="
   ) else if "%FLAG%" == "-r" (
     set FLAG_SVN_EXTERNALS_RECURSIVE=1
     set "FLAG_TEXT_SVN_IGNORE_EXTERNALS="
@@ -60,8 +78,6 @@ if not "%FLAG%" == "" (
   rem read until no flags
   goto FLAGS_LOOP
 )
-
-if %FLAG_SVN_STATUS_EXCLUDE_?% NEQ 0 set FLAG_SVN_STATUS_EXCLUDE_VERSIONED=0
 
 set "DIR_PATH_PREFIX=%~1"
 set "DIR_PATH_SUBDIR=%~2"
@@ -97,12 +113,10 @@ rem then the svn status will always report such component directories from the p
 rem So instead of call to the script you must check unversioned items in the parent path through the shell.
 
 rem always use not empty first filter
-set FINDSTR_EXP_FIRST_FILTER= ^| findstr.exe /R /C:"^[ ACDIMR?!~][ CM][ L][ +][ SX][ K][ KOTB][ C]."
+set FINDSTR_EXP_FIRST_FILTER= ^| findstr.exe /R /C:"^[ ACDIMR%FLAG_TEXT_SVN_STATUS_INCLUDE_X%%FLAG_TEXT_SVN_STATUS_INCLUDE_?%!~][ CM][ L][ +][ S%FLAG_TEXT_SVN_STATUS_INCLUDE_X%][ K][ KOTB][ C]."
 set "FINDSTR_EXP_SECOND_FILTER="
-if %FLAG_SVN_STATUS_EXCLUDE_?% NEQ 0 (
-  set FINDSTR_EXP_SECOND_FILTER= ^| findstr.exe /R /C:"^[^?]"
-) else if %FLAG_SVN_STATUS_EXCLUDE_VERSIONED% NEQ 0 (
-  set FINDSTR_EXP_SECOND_FILTER= ^| findstr.exe /R /C:"^?"
+if %FLAG_SVN_STATUS_EXCLUDE_VERSIONED% NEQ 0 (
+  set FINDSTR_EXP_SECOND_FILTER= ^| findstr.exe /R /C:"^[%FLAG_TEXT_SVN_STATUS_INCLUDE_X%%FLAG_TEXT_SVN_STATUS_INCLUDE_?%]" /C:"^....[%FLAG_TEXT_SVN_STATUS_INCLUDE_X%]"
 )
 
 set "SVN_STATUS_FILE_PATH=%DIR_PATH_PREFIX%"
