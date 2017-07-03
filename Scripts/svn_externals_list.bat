d49f2d29da97349531d6de7afc729dfcbe465c95
andry81 <andry@inbox.ru>
2017-07-03 00:59:06 +0000

    * new: all external records request direct (through the database) or indirect (through the svn command line) through the svn_externals_list.bat script * changed: all offline requests to the sqlite database through the single select * changed: replaced empty revision value from 0 to -
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/scripts/trunk@46 c83cf141-d9d7-4975-98f5-23f3ee83e49f

--- Scripts/svn_externals_list.bat
+++ Scripts/svn_externals_list.bat
@@ -22,6 +22,7 @@ if 0%SVNCMD_TOOLS_DEBUG_VERBOCITY_LVL% GEQ 3 (echo.^>^>%0 %*) >&3
 call "%%~dp0__init__.bat" || goto :EOF
 
 set "?~nx0=%~nx0"
+set "?~dp0=%~dp0"
 
 rem read the date and time
 set "DATETIME_VALUE="
@@ -61,8 +62,10 @@ set FLAG_OFFLINE=0
 set FLAG_WCROOT=0
 set FLAG_NO_URI_TRANSFORM=0
 set "FLAG_TEXT_NO_URI_TRANSFORM="
-set "FLAG_WCROOT="
+set "FLAG_TEXT_WCROOT="
 set "FLAG_TEXT_WCROOT_ABS="
+set FLAG_LOCAL_PATHS_ONLY=0
+set "FLAG_TEXT_LOCAL_PATHS_ONLY="
 
 :FLAGS_LOOP
 
@@ -77,13 +80,15 @@ if not "%FLAG%" == "" (
     set FLAG_OFFLINE=1
   ) else if "%FLAG%" == "-wcroot" (
     set FLAG_WCROOT=1
-    set "FLAG_WCROOT=%~2"
+    set "FLAG_TEXT_WCROOT=%~2"
     set "FLAG_TEXT_WCROOT_ABS=%~dpf2"
     shift
   ) else if "%FLAG%" == "-no_uri_transform" (
     set FLAG_NO_URI_TRANSFORM=1
     set "FLAG_TEXT_NO_URI_TRANSFORM= -no_uri_transform"
-    shift
+  ) else if "%FLAG%" == "-l" (
+    set FLAG_LOCAL_PATHS_ONLY=1
+    set "FLAG_TEXT_LOCAL_PATHS_ONLY= -l"
   ) else (
     echo.%?~nx0%: error: invalid flag: %FLAG%
     exit /b -255
@@ -103,55 +108,61 @@ if not exist "%BRANCH_PATH%\" (
   exit /b 255
 )
 
+set "WCROOT_PATH=%FLAG_TEXT_WCROOT%"
+set "WCROOT_PATH_ABS=%FLAG_TEXT_WCROOT_ABS%"
+
 if %FLAG_WCROOT% NEQ 0 ^
-if "%FLAG_WCROOT%" == "" (
+if "%WCROOT_PATH%" == "" (
   echo.%?~nx0%: error: SVN WC root path should not be empty.
   exit /b 254
 ) >&2
 
-if "%FLAG_WCROOT%" == "" (
-  set "FLAG_WCROOT=."
-  set "FLAG_TEXT_WCROOT_ABS=%BRANCH_PATH%"
+if "%WCROOT_PATH%" == "" (
+  set "WCROOT_PATH=."
+  set "WCROOT_PATH_ABS=%BRANCH_PATH%"
 )
 
 rem test SVN WC root path
 if %FLAG_WCROOT% NEQ 0 (
   call :TEST_WCROOT_PATH || goto :EOF
-) else set "SVN_WCROOT_PATH=%BRANCH_PATH%"
+) else (
+  set "WCROOT_PATH=%BRANCH_PATH%"
+  set "BRANCH_REL_SUB_PATH="
+)
 
 goto TEST_WCROOT_PATH_END
 
 :TEST_WCROOT_PATH
-set "SVN_WCROOT_PATH=%FLAG_TEXT_WCROOT_ABS%"
+set "WCROOT_PATH=%WCROOT_PATH_ABS%"
 
-call set "SVN_BRANCH_REL_SUB_PATH=%%BRANCH_PATH:%SVN_WCROOT_PATH%=%%"
-if not "%SVN_BRANCH_REL_SUB_PATH%" == "" (
-  if "%SVN_BRANCH_REL_SUB_PATH:~0,1%" == "\" (
-    set "SVN_BRANCH_REL_SUB_PATH=%SVN_BRANCH_REL_SUB_PATH:~1%"
+call set "BRANCH_REL_SUB_PATH=%%BRANCH_PATH:%WCROOT_PATH%=%%"
+if not "%BRANCH_REL_SUB_PATH%" == "" (
+  if "%BRANCH_REL_SUB_PATH:~0,1%" == "\" (
+    set "BRANCH_REL_SUB_PATH=%BRANCH_REL_SUB_PATH:~1%"
   )
 )
 
-if not "%SVN_BRANCH_REL_SUB_PATH%" == "" ^
-if /i not "%SVN_WCROOT_PATH%\%SVN_BRANCH_REL_SUB_PATH%" == "%BRANCH_PATH%" (
-  echo.%?~nx0%: error: SVN WC root path must be absolute and BRANCH_PATH must be descendant to the SVN WC root path: SVN_WCROOT_PATH="%SVN_WCROOT_PATH:\=/%" BRANCH_PATH="%BRANCH_PATH:\=/%".
+if not "%BRANCH_REL_SUB_PATH%" == "" ^
+if /i not "%WCROOT_PATH%\%BRANCH_REL_SUB_PATH%" == "%BRANCH_PATH%" (
+  echo.%?~nx0%: error: SVN WC root path must be absolute and BRANCH_PATH must be descendant to the SVN WC root path: WCROOT_PATH="%WCROOT_PATH:\=/%" BRANCH_PATH="%BRANCH_PATH:\=/%".
   exit /b 253
 ) >&2
 
-if not "%SVN_BRANCH_REL_SUB_PATH%" == "" set "SVN_BRANCH_REL_SUB_PATH=%SVN_BRANCH_REL_SUB_PATH:\=/%"
+if not "%BRANCH_REL_SUB_PATH%" == "" set "BRANCH_REL_SUB_PATH=%BRANCH_REL_SUB_PATH:\=/%"
 
 exit /b 0
 
 :TEST_WCROOT_PATH_END
 
-if not exist "%SVN_WCROOT_PATH%\.svn\wc.db" (
-  echo.%?~nx0%: error: SVN WC database file is not found: "%SVN_WCROOT_PATH:\=/%/.svn/wc.db"
+if not exist "%WCROOT_PATH%\.svn\wc.db" (
+  echo.%?~nx0%: error: SVN WC database file is not found: "%WCROOT_PATH:\=/%/.svn/wc.db"
   exit /b 252
 ) >&2
 
 :CHECK_WCROOT_PATH_DB_END
 
-if /i not "%SVN_WCROOT_PATH%" == "%CD%" (
-  pushd "%SVN_WCROOT_PATH%" && (
+if /i not "%WCROOT_PATH%" == "%CD%" (
+  pushd "%WCROOT_PATH%" && (
     call :IMPL
     popd
   )
@@ -160,45 +171,111 @@ if /i not "%SVN_WCROOT_PATH%" == "%CD%" (
 exit /b
 
 :IMPL
+if %FLAG_OFFLINE% EQU 0 goto IGNORE_WC_DB
 
-rem filter output only for the current directory path
-set "SQLITE_EXP_SELECT_CMD_LINE=* from new_externals "
-if %FLAG_OFFLINE% NEQ 0 ^
-if %FLAG_WCROOT% NEQ 0 (
-  if not "%SVN_BRANCH_REL_SUB_PATH%" == "" (
-    set "SQLITE_EXP_SELECT_CMD_LINE=substr(local_relpath_new, length('%SVN_BRANCH_REL_SUB_PATH%/')+1) as local_relpath_new_suffix from new_externals where substr(local_relpath_new, 1, length('%SVN_BRANCH_REL_SUB_PATH%/')) == '%SVN_BRANCH_REL_SUB_PATH%/' collate nocase and local_relpath_new_suffix != '' "
-  )
+rem check on supported wc.db user version
+call "%%?~dp0%%impl/svn_get_wc_db_user_ver.bat"
+
+if "%WC_DB_USER_VERSION%" == "" (
+  echo.%?~nx0%: error: SVN WC database user version is not set or not found: "%WCROOT_PATH:\=/%/.svn/wc.db"
+  exit /b 249
+) >&2
+
+if %WC_DB_USER_VERSION% LSS 31 (
+  echo.%?~nx0%: warning: SVN WC database user version is not supported: %WC_DB_USER_VERSION%; supported greater or equal to: 31
+) >&2
+
+if %FLAG_LOCAL_PATHS_ONLY% EQU 0 goto IGNORE_LOCAL_PATH_ONLY
+
+set "SQLITE_EXP_SELECT_FIRST_FILTER= case when kind != 'dir' then local_relpath else local_relpath || '/' end"
+set "SQLITE_EXP_WHERE_FIRST_FILTER="
+if %FLAG_WCROOT% NEQ 0 ^
+if not "%BRANCH_REL_SUB_PATH%" == "" (
+  set "SQLITE_EXP_SELECT_FIRST_FILTER= substr(case when kind != 'dir' then local_relpath else local_relpath || '/' end, length('%BRANCH_REL_SUB_PATH%/')+1)"
+  set "SQLITE_EXP_WHERE_FIRST_FILTER= and substr(local_relpath || '/', 1, length('%BRANCH_REL_SUB_PATH%/')) == '%BRANCH_REL_SUB_PATH%/' collate nocase"
 )
 
-if %FLAG_OFFLINE% NEQ 0 (
-  call "%%SQLITE_TOOLS_ROOT%%/sqlite.bat" -batch "%SVN_WCROOT_PATH%\.svn\wc.db" ".headers off" "with new_externals as ( select case when kind != 'dir' then local_relpath else local_relpath || '/' end as local_relpath_new from externals where local_relpath != '' and presence != 'not-present') select %%SQLITE_EXP_SELECT_CMD_LINE%%"
-  exit /b
+set "SQLINE_EXP_EXTERNALS_LIST=select%SQLITE_EXP_SELECT_FIRST_FILTER% from externals where local_relpath != '' and presence != 'not-present'%SQLITE_EXP_WHERE_FIRST_FILTER%"
+
+call "%%SQLITE_TOOLS_ROOT%%/sqlite.bat" -batch "%%WCROOT_PATH%%\.svn\wc.db" ".headers off" "%%SQLINE_EXP_EXTERNALS_LIST%%"
+
+exit /b
+
+:IGNORE_LOCAL_PATH_ONLY
+
+set "SQLITE_EXP_WHERE_FIRST_FILTER="
+if %FLAG_WCROOT% NEQ 0 ^
+if not "%BRANCH_REL_SUB_PATH%" == "" (
+  set "SQLITE_EXP_WHERE_FIRST_FILTER= and substr(local_relpath || '/', 1, length('%BRANCH_REL_SUB_PATH%/')) == '%BRANCH_REL_SUB_PATH%/' collate nocase"
 )
 
 if %FLAG_NO_URI_TRANSFORM% NEQ 0 goto IGNORE_URI_TRANSFORM
 
-svn info . --non-interactive > "%INFO_FILE_TMP%" || exit /b 251
+set "SQLINE_EXP_EXTERNALS_LIST=select case when def_local_relpath != '' then def_local_relpath else '.' end as local_prefix, case when def_local_relpath != '' then substr(case when kind != 'dir' then local_relpath else local_relpath || '/' end, length(def_local_relpath)+2) else case when kind != 'dir' then local_relpath else local_relpath || '/' end end as external_path, case when def_revision != '' then def_revision else '-' end as operative_rev, case when def_operational_revision != '' then def_operational_revision else '-' end as peg_rev, repos_id, def_repos_relpath from externals where local_relpath != '' and presence != 'not-present'%SQLITE_EXP_WHERE_FIRST_FILTER%"
+
+for /F "usebackq eol=	 tokens=1,2,3,4,5,6 delims=|" %%i in (`@call "%%SQLITE_TOOLS_ROOT%%/sqlite.bat" -batch "%%WCROOT_PATH%%\.svn\wc.db" ".headers off" ".mode list" ".separator |" ".nullvalue ." "%%SQLINE_EXP_EXTERNALS_LIST%%"`) do (
+  set "LOCAL_PREFIX=%%i"
+  set "EXTERNAL_PATH=%%j"
+  set "OPERATIVE_REV=%%k"
+  set "PEG_REV=%%l"
+  set "REPOS_ID=%%m"
+  set "REPO_RELPATH=%%n"
+  call :PROCESS_EXTERNAL_RECORD || goto :EOF
+)
+
+exit /b 0
+
+:PROCESS_EXTERNAL_RECORD
+set "REPOROOT="
+for /F "usebackq eol=	 tokens=* delims=" %%i in (`@call "%%SQLITE_TOOLS_ROOT%%/sqlite.bat" -batch "%%WCROOT_PATH%%\.svn\wc.db" ".headers off" "select root from repository where id='%%REPOS_ID%%'"`) do set "REPOROOT=%%i"
+if "%REPOROOT%" == "" (
+  echo.%?~nx0%: error: SVN database `REPOSITORY root` request has failed: "%WCROOT_PATH:\=/%/.svn/wc.db".
+  exit /b 240
+) >&2
+
+set "REPOPATH=%REPOROOT%"
+if not "%REPO_RELPATH%" == "" set "REPOPATH=%REPOPATH%/%REPO_RELPATH%"
+
+rem special form of the echo command to ignore special characters in the echo value.
+for /F "eol=	 tokens=* delims=" %%i in ("%LOCAL_PREFIX%|%EXTERNAL_PATH%|%OPERATIVE_REV%|%PEG_REV%|%REPOPATH%") do (echo.%%i)
+
+exit /b
+
+:IGNORE_URI_TRANSFORM
+
+set "SQLINE_EXP_EXTERNALS_LIST=select case when def_local_relpath != '' then def_local_relpath else '.' end as local_prefix, case when def_local_relpath != '' then substr(case when kind != 'dir' then local_relpath else local_relpath || '/' end, length(def_local_relpath)+2) else case when kind != 'dir' then local_relpath else local_relpath || '/' end end as external_path, case when def_revision != '' then def_revision else '-' end as operative_rev, case when def_operational_revision != '' then def_operational_revision else '-' end as peg_rev, '-' from externals where local_relpath != '' and presence != 'not-present'%SQLITE_EXP_WHERE_FIRST_FILTER%"
+
+call "%%SQLITE_TOOLS_ROOT%%/sqlite.bat" -batch "%%WCROOT_PATH%%\.svn\wc.db" ".headers off" ".mode list" ".separator |" ".nullvalue ." "%%SQLINE_EXP_EXTERNALS_LIST%%"
+
+exit /b
+
+:IGNORE_WC_DB
+
+if %FLAG_NO_URI_TRANSFORM% NEQ 0 goto IGNORE_URI_TRANSFORM
+if %FLAG_LOCAL_PATHS_ONLY% NEQ 0 goto IGNORE_URI_TRANSFORM
+
+svn info . --non-interactive > "%INFO_FILE_TMP%" || exit /b 248
 
 call "%%SVNCMD_TOOLS_ROOT%%/extract_info_param.bat" "%%INFO_FILE_TMP%%" "URL"
 set "BRANCH_DIR_URL=%RETURN_VALUE%"
 if "%BRANCH_DIR_URL%" == "" (
   echo.%?~nx0%: error: `URL` property is not found in SVN info file: "%INFO_FILE_TMP%".
-  exit /b 250
+  exit /b 230
 ) >&2
 
 call "%%SVNCMD_TOOLS_ROOT%%/extract_info_param.bat" "%%INFO_FILE_TMP%%" "Repository Root"
 set "BRANCH_REPO_ROOT=%RETURN_VALUE%"
 if "%BRANCH_REPO_ROOT%" == "" (
   echo.%?~nx0%: error: `Repository Root` property is not found in SVN info file: "%BRANCH_INFO_FILE%".
-  exit /b 249
+  exit /b 229
 ) >&2
 
 :IGNORE_URI_TRANSFORM
 rem from externals
-svn pget svn:externals . -R --non-interactive > "%EXTERNALS_FILE_TMP%" || exit /b 248
+svn pget svn:externals . -R --non-interactive > "%EXTERNALS_FILE_TMP%" || exit /b 228
 
-rem TODO:
-rem 1. add `-prefix_path "<prefix_path>"` flag to the gen_externals_list_from_pget.bat script to ignore externals with different <prefix_path> path
+set "CMD_LINE_PREFIX_PATH="
+if not "%BRANCH_REL_SUB_PATH%" == "" set CMD_LINE_PREFIX_PATH= -prefix_path "%BRANCH_REL_SUB_PATH%"
 
 rem convert externals into CSV list
-call "%%SVNCMD_TOOLS_ROOT%%/gen_externals_list_from_pget.bat"%%FLAG_TEXT_NO_URI_TRANSFORM%% "%%EXTERNALS_FILE_TMP%%" "%%BRANCH_REPO_ROOT%%" "%%BRANCH_DIR_URL%%" || exit /b 247
+call "%%SVNCMD_TOOLS_ROOT%%/gen_externals_list_from_pget.bat"%%FLAG_TEXT_NO_URI_TRANSFORM%%%%FLAG_TEXT_LOCAL_PATHS_ONLY%%%%CMD_LINE_PREFIX_PATH%% "%%EXTERNALS_FILE_TMP%%" "%%BRANCH_REPO_ROOT%%" "%%BRANCH_DIR_URL%%" || exit /b 245
