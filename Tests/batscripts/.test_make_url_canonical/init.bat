39bc65468360414528343c5c0d6c839072b02a35
andry81 <andry@inbox.ru>
2017-08-12 19:22:05 +0000

    * batscript tests update
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/scripts/trunk@63 c83cf141-d9d7-4975-98f5-23f3ee83e49f

---
+++ Tests/batscripts/.test_make_url_canonical/init.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set "STRING_REFERENCE=%~1"
+set "URL=%~2"
+
+exit /b 0
--- Tests/batscripts/test_all.bat
+++ Tests/batscripts/test_all.bat
@@ -3,17 +3,14 @@
 rem Create local variable's stack
 setlocal
 
-call "%%~dp0__init__.bat"
-
-set /A __NEST_LVL+=1
+call "%%~dp0__init__.bat" || goto :EOF
+call "%%TESTLIB_ROOT%%/init.bat" "%%~dpf0" || goto :EOF
 
 call "%%TESTS_ROOT%%/test_make_url_canonical.bat"
 call "%%TESTS_ROOT%%/test_make_url_absolute.bat"
 
-set /A __NEST_LVL-=1
+rem WARNING: must be called without the call prefix!
+"%TESTLIB_ROOT%/exit.bat"
 
-if %__NEST_LVL%0 EQU 0 (
-  echo    %__PASSED_TESTS% of %__OVERALL_TESTS% tests is passed.
-  echo.^
-  pause
-)
+rem no code can be executed here, just in case
+exit /b
