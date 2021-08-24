8f500fcb5215eea178c4c2c06e580764b1ba20c4
andry81 <andry@inbox.ru>
2021-08-24 22:15:46 +0000

    2021.08.25: * new: __init__: contools-svncmd cross initialization
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/scripts/trunk@104 c83cf141-d9d7-4975-98f5-23f3ee83e49f

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -9,20 +9,12 @@ if not defined NEST_LVL set NEST_LVL=0
 if not defined SVNCMD_PROJECT_ROOT                call :CANONICAL_PATH SVNCMD_PROJECT_ROOT                "%%~dp0.."
 if not defined SVNCMD_PROJECT_EXTERNALS_ROOT      call :CANONICAL_PATH SVNCMD_PROJECT_EXTERNALS_ROOT      "%%SVNCMD_PROJECT_ROOT%%/_externals"
 
-if not defined CONTOOLS_ROOT                      call :CANONICAL_PATH CONTOOLS_ROOT                      "%%SVNCMD_PROJECT_EXTERNALS_ROOT%%/contools/Scripts/Tools"
-if not defined CONTOOLS_BUILD_TOOLS_ROOT          call :CANONICAL_PATH CONTOOLS_BUILD_TOOLS_ROOT          "%%CONTOOLS_ROOT%%/build"
-if not defined CONTOOLS_SQLITE_TOOLS_ROOT         call :CANONICAL_PATH CONTOOLS_SQLITE_TOOLS_ROOT         "%%CONTOOLS_ROOT%%/sqlite"
-if not defined CONTOOLS_TESTLIB_ROOT              call :CANONICAL_PATH CONTOOLS_TESTLIB_ROOT              "%%CONTOOLS_ROOT%%/testlib"
-if not defined CONTOOLS_XML_TOOLS_ROOT            call :CANONICAL_PATH CONTOOLS_XML_TOOLS_ROOT            "%%CONTOOLS_ROOT%%/xml"
-if not defined CONTOOLS_VARS_ROOT                 call :CANONICAL_PATH CONTOOLS_VARS_ROOT                 "%%CONTOOLS_ROOT%%/vars"
-
 if not defined SVNCMD_TOOLS_ROOT                  call :CANONICAL_PATH SVNCMD_TOOLS_ROOT                  "%%SVNCMD_PROJECT_ROOT%%/Scripts"
 
-if not defined CONTOOLS_UTILITIES_ROOT            call :CANONICAL_PATH CONTOOLS_UTILITIES_ROOT            "%%SVNCMD_PROJECT_EXTERNALS_ROOT%%/contools/Utilities"
-if not defined CONTOOLS_UTILITIES_BIN_ROOT        call :CANONICAL_PATH CONTOOLS_UTILITIES_BIN_ROOT        "%%CONTOOLS_UTILITIES_ROOT%%/bin"
-if not defined CONTOOLS_GNUWIN32_ROOT             call :CANONICAL_PATH CONTOOLS_GNUWIN32_ROOT             "%%CONTOOLS_UTILITIES_BIN_ROOT%%/gnuwin32"
-if not defined CONTOOLS_UTILITIES_HASHDEEP_ROOT   call :CANONICAL_PATH CONTOOLS_UTILITIES_HASHDEEP_ROOT   "%%CONTOOLS_UTILITIES_BIN_ROOT%%/hashdeep"
-if not defined CONTOOLS_UTILITIES_SQLITE_ROOT     call :CANONICAL_PATH CONTOOLS_UTILITIES_SQLITE_ROOT     "%%CONTOOLS_UTILITIES_BIN_ROOT%%/sqlite"
+rem init contools project
+if exist "%SVNCMD_PROJECT_EXTERNALS_ROOT%/contools/__init__/__init__.bat" (
+  call "%%SVNCMD_PROJECT_EXTERNALS_ROOT%%/contools/__init__/__init__.bat" || exit /b
+)
 
 exit /b 0
 
--- __init__/__init__.sh
+++ __init__/__init__.sh
@@ -14,19 +14,11 @@ SVNCMD_PROJECT_ROOT="$RETURN_VALUE"
 
 [[ -z "$SVNCMD_PROJECT_EXTERNALS_ROOT" ]] &&      tkl_export SVNCMD_PROJECT_EXTERNALS_ROOT      "$SVNCMD_PROJECT_ROOT/_externals"
 
-[[ -z "$CONTOOLS_ROOT" ]] &&                      tkl_export CONTOOLS_ROOT                      "$SVNCMD_PROJECT_EXTERNALS_ROOT/contools/Scripts/Tools"
-[[ -z "$CONTOOLS_BUILD_TOOLS_ROOT" ]] &&          tkl_export CONTOOLS_BUILD_TOOLS_ROOT          "$CONTOOLS_ROOT/build"
-[[ -z "$CONTOOLS_SQLITE_TOOLS_ROOT" ]] &&         tkl_export CONTOOLS_SQLITE_TOOLS_ROOT         "$CONTOOLS_ROOT/sqlite"
-[[ -z "$CONTOOLS_TESTLIB_ROOT" ]] &&              tkl_export CONTOOLS_TESTLIB_ROOT              "$CONTOOLS_ROOT/testlib"
-[[ -z "$CONTOOLS_XML_TOOLS_ROOT" ]] &&            tkl_export CONTOOLS_XML_TOOLS_ROOT            "$CONTOOLS_ROOT/xml"
-[[ -z "$CONTOOLS_VARS_ROOT" ]] &&                 tkl_export CONTOOLS_VARS_ROOT                 "$CONTOOLS_ROOT/vars"
-
 [[ -z "$SVNCMD_TOOLS_ROOT" ]] &&                  tkl_export SVNCMD_TOOLS_ROOT                  "$SVNCMD_PROJECT_ROOT/Scripts"
 
-[[ -z "$CONTOOLS_UTILITIES_ROOT" ]] &&            tkl_export CONTOOLS_UTILITIES_ROOT            "$SVNCMD_PROJECT_EXTERNALS_ROOT/contools/Utilities"
-[[ -z "$CONTOOLS_UTILITIES_BIN_ROOT" ]] &&        tkl_export CONTOOLS_UTILITIES_BIN_ROOT        "$CONTOOLS_UTILITIES_ROOT/bin"
-[[ -z "$CONTOOLS_GNUWIN32_ROOT" ]] &&             tkl_export CONTOOLS_GNUWIN32_ROOT             "$CONTOOLS_UTILITIES_BIN_ROOT/gnuwin32"
-[[ -z "$CONTOOLS_UTILITIES_HASHDEEP_ROOT" ]] &&   tkl_export CONTOOLS_UTILITIES_HASHDEEP_ROOT   "$CONTOOLS_UTILITIES_BIN_ROOT/hashdeep"
-[[ -z "$CONTOOLS_UTILITIES_SQLITE_ROOT" ]] &&     tkl_export CONTOOLS_UTILITIES_SQLITE_ROOT     "$CONTOOLS_UTILITIES_BIN_ROOT/sqlite"
+# init contools project
+if [[ -f "$SVNCMD_PROJECT_EXTERNALS_ROOT/contools/__init__/__init__.sh" ]]; then
+  tkl_include "$SVNCMD_PROJECT_EXTERNALS_ROOT/contools/__init__/__init__.sh" || tkl_abort_include
+fi
 
 return 0
