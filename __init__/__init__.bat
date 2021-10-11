ed47a1b0820c34b8c6f8b9535de8971800a17243
andry81 <andry@inbox.ru>
2021-10-11 10:34:03 +0000

    2021.10.11: * changed: __init__: `chcp.com` usage replaced by `chcp.bat` script
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/scripts/trunk@119 c83cf141-d9d7-4975-98f5-23f3ee83e49f

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -34,7 +34,7 @@ if exist "%SVNCMD_PROJECT_EXTERNALS_ROOT%/tacklelib/__init__/__init__.bat" (
 if not exist "%PROJECT_OUTPUT_ROOT%\" ( mkdir "%PROJECT_OUTPUT_ROOT%" || exit /b 11 )
 if not exist "%PROJECT_LOG_ROOT%\" ( mkdir "%PROJECT_LOG_ROOT%" || exit /b 12 )
 
-if defined CHCP chcp %CHCP%
+if defined CHCP call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%CHCP%%
 
 exit /b 0
 
