5e9a11cc0390beffb6383d54dd31bb06787fa1e2
andry81 <andry@inbox.ru>
2021-12-15 06:59:27 +0000

    2021.12.15: * changed: __init__: execution speed optimization
    git-svn-id: svn+ssh://svn.code.sf.net/p/svncmd/scripts/trunk@137 c83cf141-d9d7-4975-98f5-23f3ee83e49f

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -6,14 +6,14 @@ set "SVNCMD_PROJECT_ROOT_INIT0_DIR=%~dp0"
 
 if not defined NEST_LVL set NEST_LVL=0
 
-if not defined SVNCMD_PROJECT_ROOT                call :CANONICAL_PATH SVNCMD_PROJECT_ROOT                "%%~dp0.."
-if not defined SVNCMD_PROJECT_EXTERNALS_ROOT      call :CANONICAL_PATH SVNCMD_PROJECT_EXTERNALS_ROOT      "%%SVNCMD_PROJECT_ROOT%%/_externals"
+if not defined SVNCMD_PROJECT_ROOT                call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_ROOT                "%%~dp0.."
+if not defined SVNCMD_PROJECT_EXTERNALS_ROOT      call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_EXTERNALS_ROOT      "%%SVNCMD_PROJECT_ROOT%%/_externals"
 
-if not defined PROJECT_OUTPUT_ROOT                call :CANONICAL_PATH PROJECT_OUTPUT_ROOT                "%%SVNCMD_PROJECT_ROOT%%/_out"
-if not defined PROJECT_LOG_ROOT                   call :CANONICAL_PATH PROJECT_LOG_ROOT                   "%%SVNCMD_PROJECT_ROOT%%/.log"
+if not defined PROJECT_OUTPUT_ROOT                call "%%~dp0canonical_path.bat" PROJECT_OUTPUT_ROOT                "%%SVNCMD_PROJECT_ROOT%%/_out"
+if not defined PROJECT_LOG_ROOT                   call "%%~dp0canonical_path.bat" PROJECT_LOG_ROOT                   "%%SVNCMD_PROJECT_ROOT%%/.log"
 
-if not defined SVNCMD_PROJECT_INPUT_CONFIG_ROOT   call :CANONICAL_PATH SVNCMD_PROJECT_INPUT_CONFIG_ROOT   "%%SVNCMD_PROJECT_ROOT%%/_config"
-if not defined SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT  call :CANONICAL_PATH SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT  "%%PROJECT_OUTPUT_ROOT%%/config/svncmd"
+if not defined SVNCMD_PROJECT_INPUT_CONFIG_ROOT   call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_INPUT_CONFIG_ROOT   "%%SVNCMD_PROJECT_ROOT%%/_config"
+if not defined SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT  call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT  "%%PROJECT_OUTPUT_ROOT%%/config/svncmd"
 
 if not exist "%SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT%\" ( mkdir "%SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT%" || exit /b 10 )
 
@@ -36,14 +36,4 @@ if not exist "%PROJECT_LOG_ROOT%\" ( mkdir "%PROJECT_LOG_ROOT%" || exit /b 12 )
 
 if defined CHCP call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%CHCP%%
 
-exit /b 0
-
-:CANONICAL_PATH
-setlocal DISABLEDELAYEDEXPANSION
-for /F "eol= tokens=* delims=" %%i in ("%~2\.") do set "RETURN_VALUE=%%~fi"
-rem set "RETURN_VALUE=%RETURN_VALUE:\=/%"
-(
-  endlocal
-  set "%~1=%RETURN_VALUE%"
-)
 exit /b 0
