048c4f7003dfcef9096ea153eddad99636139d4d
andry81 <andry@inbox.ru>
2023-12-09 11:17:59 +0300

    2023.12.09: * fixed: __init__/__init__.bat: execution fixup

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -6,8 +6,25 @@ set "SVNCMD_PROJECT_ROOT_INIT0_DIR=%~dp0"
 
 if not defined NEST_LVL set NEST_LVL=0
 
+rem Do not make a file or a directory
+if defined NO_GEN set /A NO_GEN+=0
+
+rem Do not make a log directory or a log file
+if defined NO_LOG set /A NO_LOG+=0
+
+rem Do not make a log output or stdio duplication into files
+if defined NO_LOG_OUTPUT set /A NO_LOG_OUTPUT+=0
+
+rem Do not change code page
+if defined NO_CHCP set /A NO_CHCP+=0
+
 if not defined SVNCMD_PROJECT_ROOT                call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_ROOT                "%%~dp0.."
 if not defined SVNCMD_PROJECT_EXTERNALS_ROOT      call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_EXTERNALS_ROOT      "%%SVNCMD_PROJECT_ROOT%%/_externals"
+
+if not exist "%SVNCMD_PROJECT_EXTERNALS_ROOT%\*" (
+  echo.%~nx0: error: SVNCMD_PROJECT_EXTERNALS_ROOT directory does not exist: "%SVNCMD_PROJECT_EXTERNALS_ROOT%".
+  exit /b 255
+) >&2
 
 if not defined PROJECT_OUTPUT_ROOT                call "%%~dp0canonical_path.bat" PROJECT_OUTPUT_ROOT                "%%SVNCMD_PROJECT_ROOT%%/_out"
 if not defined PROJECT_LOG_ROOT                   call "%%~dp0canonical_path.bat" PROJECT_LOG_ROOT                   "%%SVNCMD_PROJECT_ROOT%%/.log"
@@ -15,17 +32,21 @@ if not defined PROJECT_LOG_ROOT                   call "%%~dp0canonical_path.bat
 if not defined SVNCMD_PROJECT_INPUT_CONFIG_ROOT   call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_INPUT_CONFIG_ROOT   "%%SVNCMD_PROJECT_ROOT%%/_config"
 if not defined SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT  call "%%~dp0canonical_path.bat" SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT  "%%PROJECT_OUTPUT_ROOT%%/config/svncmd"
 
+rem retarget externals of an external project
+
+if not defined CONTOOLS_PROJECT_EXTERNALS_ROOT    call "%%~dp0canonical_path.bat" CONTOOLS_PROJECT_EXTERNALS_ROOT    "%%SVNCMD_PROJECT_EXTERNALS_ROOT%%"
+
+rem init immediate external projects
+
+if exist "%TACKLEBAR_PROJECT_EXTERNALS_ROOT%/contools/__init__/__init__.bat" (
+  call "%%TACKLEBAR_PROJECT_EXTERNALS_ROOT%%/contools/__init__/__init__.bat" -no_load_user_config || exit /b
+)
+
 if %NO_GEN%0 EQU 0 (
-  if not exist "%SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT%\" ( mkdir "%SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT%" || exit /b 10 )
+  call "%%CONTOOLS_ROOT%%/std/mkdir_if_notexist.bat" "%SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT%" || exit /b 10
 )
 
 if not defined LOAD_CONFIG_VERBOSE if %INIT_VERBOSE%0 NEQ 0 set LOAD_CONFIG_VERBOSE=1
-
-rem init contools first
-
-if exist "%SVNCMD_PROJECT_EXTERNALS_ROOT%/contools/__init__/__init__.bat" (
-  call "%%SVNCMD_PROJECT_EXTERNALS_ROOT%%/contools/__init__/__init__.bat" %%* || exit /b
-)
 
 if %NO_GEN%0 EQU 0 (
   call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* -lite_parse -gen_user_config "%%SVNCMD_PROJECT_INPUT_CONFIG_ROOT%%" "%%SVNCMD_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
@@ -39,12 +60,10 @@ if exist "%SVNCMD_PROJECT_EXTERNALS_ROOT%/tacklelib/__init__/__init__.bat" (
 
 if %NO_GEN%0 EQU 0 (
   call "%%CONTOOLS_ROOT%%/std/mkdir_if_notexist.bat" "%%PROJECT_OUTPUT_ROOT%%" || exit /b 11
-
-  if %NO_LOG%0 EQU 0 (
-    call "%%CONTOOLS_ROOT%%/std/mkdir_if_notexist.bat" "%%PROJECT_LOG_ROOT%%" || exit /b 12
-  )
 )
 
-if defined CHCP call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%CHCP%%
+if %NO_CHCP%0 EQU 0 (
+  if defined CHCP call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%CHCP%%
+)
 
 exit /b 0
